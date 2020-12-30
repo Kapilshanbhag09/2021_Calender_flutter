5644c757ad20c9a63e062f5de86a9f8e3282cf69
Kapilshanbhag09 <shanbhag.kapil09@gmail.com>
2020-12-30 11:47:56 +0530

    Corrected june event

--- lib/screens/june.dart
+++ lib/screens/june.dart
@@ -589,6 +589,7 @@ class June extends StatelessWidget {
               margin: const EdgeInsets.only(bottom: 10.0),
               child: Column(
                 children: <Widget>[
+                  //New Moon
                   Container(
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10.0),
@@ -596,7 +597,41 @@ class June extends StatelessWidget {
                       ),
                       width: MediaQuery.of(context).size.width,
                       margin: const EdgeInsets.only(
-                          left: 10.0, right: 10.0, top: 10.0, bottom: 10.0),
+                          left: 10.0, right: 10.0, top: 10.0),
+                      child: Row(children: <Widget>[
+                        Container(
+                          margin: const EdgeInsets.only(top: 7.0, bottom: 7.0),
+                          width: MediaQuery.of(context).size.width / 6.0,
+                          alignment: Alignment.center,
+                          child: Text(
+                            "10",
+                            style: TextStyle(
+                                fontSize: 30.0,
+                                color: Color(0xFF23A7A9),
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          padding: EdgeInsets.only(left: 10.0),
+                          child: Text(
+                            "New Moon",
+                            style: TextStyle(
+                                fontSize: 25.0,
+                                color: Color(0xFF23A7A9),
+                                fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ])),
+                  //International Yoga Day
+                  Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.white,
+                      ),
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(
+                          left: 10.0, right: 10.0, top: 10.0),
                       child: Row(children: <Widget>[
                         Container(
                           margin: const EdgeInsets.only(top: 7.0, bottom: 7.0),
@@ -615,6 +650,40 @@ class June extends StatelessWidget {
                           padding: EdgeInsets.only(left: 10.0),
                           child: Text(
                             "International Yoga Day",
+                            style: TextStyle(
+                                fontSize: 25.0,
+                                color: Color(0xFF23A7A9),
+                                fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ])),
+                  //Full Moon
+                  Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.white,
+                      ),
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(
+                          left: 10.0, right: 10.0, top: 10.0, bottom: 10.0),
+                      child: Row(children: <Widget>[
+                        Container(
+                          margin: const EdgeInsets.only(top: 7.0, bottom: 7.0),
+                          width: MediaQuery.of(context).size.width / 6.0,
+                          alignment: Alignment.center,
+                          child: Text(
+                            "25",
+                            style: TextStyle(
+                                fontSize: 30.0,
+                                color: Color(0xFF23A7A9),
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          padding: EdgeInsets.only(left: 10.0),
+                          child: Text(
+                            "Full Moon",
                             style: TextStyle(
                                 fontSize: 25.0,
                                 color: Color(0xFF23A7A9),
